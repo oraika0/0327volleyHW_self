7facaf16514f73e6cbf4192f37f6503fffa8a6dc
oraika0 <kai91131@gmail.com>
2023-03-29 13:41:17 +0800

    3

--- app/src/main/java/com/example/a0327volleyhw_self/MainActivity.java
+++ app/src/main/java/com/example/a0327volleyhw_self/MainActivity.java
@@ -7,6 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.HorizontalScrollView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -27,36 +31,38 @@ import java.nio.charset.StandardCharsets;
 
 
 public class MainActivity extends AppCompatActivity {
+    TableLayout tableLayout;
     EditText et;
     String filename = "out.csv";
     String url="https://web.cs.wpi.edu/~cs1004/a16/Resources/SacramentoRealEstateTransactions.csv\n";
+
     private void sendAndRequestResponse(){
         RequestQueue mRequestQueue = Volley.newRequestQueue(this);
         StringRequest mStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                et.setText(response.toString());
+                //  et.setText(response.toString());
                 try {
                     FileOutputStream outfile = openFileOutput(filename, Context.MODE_APPEND);
                     outfile.write(response.getBytes(StandardCharsets.UTF_8));
                     outfile.close();
                     parsing_csvFile();
-                }catch (FileNotFoundException e) {
+                } catch (FileNotFoundException e) {
                     throw new RuntimeException(e);
-                }catch (IOException e){
+                } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
             }
-        },new Response.ErrorListener(){
+        }, new Response.ErrorListener() {
             public void onErrorResponse(VolleyError error){
             }
         });
         mRequestQueue.add(mStringRequest);
 
     }
-    private void parsing_csvFile(){
+
+    private void parsing_csvFile() {
         try {
-            System.out.println("\n");
             File file = getBaseContext().getFileStreamPath("out.csv");
             FileInputStream fstream = new FileInputStream(file);
             DataInputStream in = new DataInputStream(fstream);
@@ -64,21 +70,34 @@ public class MainActivity extends AppCompatActivity {
             String line = "";
             String spiltBy = ",";
 
-            while((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 String[] apartment = line.split(spiltBy);
-                System.out.println("street"+apartment[0]+", city = " + apartment[1] + ", zip = " + apartment[2] + ", state = " + apartment[3] + "beds = " + apartment[4]);
+                TableRow row = new TableRow(this);
+
+                for (int ct = 0; ct < apartment.length; ct++) {
+                    TextView tv = new TextView(this);
+                    tv.setText(apartment[ct]);
+                    row.addView(tv);
+                }
+
+                tableLayout.addView(row);
+                System.out.println("street" + apartment[0] + ", city = " + apartment[1] + ", zip = " + apartment[2] + ", state = " + apartment[3] + "beds = " + apartment[4]);
             }
-        }catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
-        }catch (IOException e){
+        } catch (IOException e) {
             throw new RuntimeException(e);
+        } catch (OutOfMemoryError e) {
+            throw new RuntimeException("CSV file too large to parse.", e);
         }
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tableLayout = (TableLayout)findViewById(R.id.tl);
         et = (EditText)findViewById(R.id.et);
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
